4e3046bfc3bc4304489a9d2589de0b8f27a9d047
MasonProtter <mason.protter@icloud.com>
2025-02-26 21:06:52 +0100

    support computed properties with inputs

--- src/symbolic_indexing.jl
+++ src/symbolic_indexing.jl
@@ -14,6 +14,12 @@ struct ParamIndex #todo: this'll require some generalization to support weight p
     v_index::Int
     prop::Symbol
 end
+struct CompuIndex #todo: this'll require some generalization to support weight params
+    tup_index::Int
+    v_index::Int
+    prop::Symbol
+    requires_inputs::Bool
+end
 
 function make_state_namemap(names_partitioned::NTuple{N, Vector{Symbol}},
                             states_partitioned::NTuple{N, AbstractVector{<:SubsystemStates}}) where {N}
@@ -50,15 +56,21 @@ end
 function make_compu_namemap(names_partitioned::NTuple{N, Vector{Symbol}},
                           states_partitioned::NTuple{N, AbstractVector{<:SubsystemStates}},
                           params_partitioned::NTuple{N, AbstractVector{<:SubsystemParams}}) where {N}
-    namemap = OrderedDict{Symbol, ParamIndex}()
+    namemap = OrderedDict{Symbol, CompuIndex}()
     for i ∈ eachindex(names_partitioned, states_partitioned, params_partitioned)
         for j ∈ eachindex(names_partitioned[i], states_partitioned[i], params_partitioned[i])
             states = states_partitioned[i][j]
             params = params_partitioned[i][j]
             sys = Subsystem(states, params)
             for name ∈ keys(computed_properies(sys))
+                requires_inputs = false
                 propname = Symbol(names_partitioned[i][j], "₊", name)
-                namemap[propname] = ParamIndex(i, j, name)
+                namemap[propname] = CompuIndex(i, j, name, requires_inputs)
+            end
+            for name ∈ keys(computed_properties_with_inputs(sys))
+                requires_inputs = true
+                propname = Symbol(names_partitioned[i][j], "₊", name)
+                namemap[propname] = CompuIndex(i, j, name, requires_inputs)
             end
         end
     end
@@ -141,20 +153,40 @@ function SymbolicIndexingInterface.is_time_dependent(sys::GraphSystem)
     true
 end
 
+function SymbolicIndexingInterface.observed(f::ODEFunction{a, b, F}, sym::Symbol) where {a, b, F<:GraphSystemFunction}
+    observed(f.f.sys, sym)
+end
+
 function SymbolicIndexingInterface.is_observed(sys::GraphSystem, sym)
     haskey(sys.compu_namemap, sym)
 end
 
-function SymbolicIndexingInterface.observed(f::ODEFunction{a, b, F}, sym::Symbol) where {a, b, F<:GraphSystemFunction}
-    observed(f.f.sys, sym)
-end
 function SymbolicIndexingInterface.observed(sys::GraphSystem, sym)
-    (; tup_index, v_index, prop) = sys.compu_namemap[sym]
-    function gen(u, p, t)
-        (; params_partitioned, state_types_val) = p
-        states_partitioned = to_vec_o_states(u.x, state_types_val)
-        subsys = Subsystem(states_partitioned[tup_index][v_index], params_partitioned[tup_index][v_index])
-        getproperty(subsys, prop)
+    (; tup_index, v_index, prop, requires_inputs) = sys.compu_namemap[sym]
+    
+    # lift these to the type domain so that we specialize on them in the returned closures
+    val_tup_index = Val(tup_index)
+    val_prop = Val(prop)
+    
+    if requires_inputs
+        function (u, p, t)
+            (; params_partitioned, state_types_val, connection_matrices) = p
+            states_partitioned = to_vec_o_states(u.x, state_types_val)
+            i = valueof(val_tup_index)
+            subsys = Subsystem(states_partitioned[i][v_index], params_partitioned[i][v_index])
+            input = calculate_inputs(val_tup_index, v_index, states_partitioned, params_partitioned, connection_matrices)
+
+            comp_props = computed_properties_with_inputs(subsys)
+            comp_props[prop](subsys, input)
+        end
+    else
+        function (u, p, t)
+            (; params_partitioned, state_types_val) = p
+            states_partitioned = to_vec_o_states(u.x, state_types_val)
+            i = valueof(val_tup_index)
+            subsys = Subsystem(states_partitioned[i][v_index], params_partitioned[i][v_index])
+            getproperty(subsys, prop)
+        end
     end
 end
 
