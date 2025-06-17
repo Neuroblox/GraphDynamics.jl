e9929b30abf9ebe1e62d6987019d42fef48a219a
Mason Protter <mason.protter@icloud.com>
2025-06-17 11:40:37 +0200

    fix Symbolic Indexing with `Num`s.

--- ext/MTKExt.jl
+++ ext/MTKExt.jl
@@ -2,26 +2,26 @@ module MTKExt
 
 using ModelingToolkit: ModelingToolkit, Num
 using Symbolics: Symbolics, tosymbol
-using GraphDynamics: GraphDynamics, GraphSystem
+using GraphDynamics: GraphDynamics, GraphSystemParameters, PartitionedGraphSystem
 using SymbolicIndexingInterface: SymbolicIndexingInterface
 
-function SymbolicIndexingInterface.is_variable(sys::GraphSystem, var::Num)
+function SymbolicIndexingInterface.is_variable(sys::PartitionedGraphSystem, var::Num)
     SymbolicIndexingInterface.is_variable(sys, tosymbol(var; escape=false))
 end
 
-function SymbolicIndexingInterface.variable_index(sys::GraphSystem, var::Num)
+function SymbolicIndexingInterface.variable_index(sys::PartitionedGraphSystem, var::Num)
     SymbolicIndexingInterface.variable_index(sys, tosymbol(var; escape=false))
 end
 
-function SymbolicIndexingInterface.is_parameter(sys::GraphSystem, var::Num)
+function SymbolicIndexingInterface.is_parameter(sys::PartitionedGraphSystem, var::Num)
     SymbolicIndexingInterface.is_parameter(sys, tosymbol(var; escape=false))
 end
 
-function SymbolicIndexingInterface.parameter_index(sys::GraphSystem, var::Num)
+function SymbolicIndexingInterface.parameter_index(sys::PartitionedGraphSystem, var::Num)
     SymbolicIndexingInterface.parameter_index(sys, tosymbol(var; escape=false))
 end
 
-function SymbolicIndexingInterface.is_independent_variable(sys::GraphSystem, var::Num)
+function SymbolicIndexingInterface.is_independent_variable(sys::PartitionedGraphSystem, var::Num)
     SymbolicIndexingInterface.is_independent_variable(sys, tosymbol(var; escape=false))
 end
 
